5a73e7a17d23a54e817bdd71148a944328e4a915
Immanuel Z. Umali <138665333+Immern@users.noreply.github.com>
2024-11-06 01:01:20 +0800

    Working draft of inference engine

--- src/main/adventure_world.pl
+++ src/main/adventure_world.pl
@@ -1,16 +1,40 @@
 % Knowledge Base
-
+% Test using example map from specs
+gold(1, 2).
+gold(2, 1).
+gold(4, 5).
+gold(5, 1).
+pit(2, 3).
+pit(3, 2).
+pit(4, 4).
 
 % Sensors
-is_safe() :-
-    todo.
+is_safe(X, Y) :-
+    \+ pit(X, Y).
+
+is_home(X, Y) :-
+    \+ gold(X, Y),
+    \+ breeze(X, Y),
+    is_safe(X, Y).
+
+is_pit(X, Y) :-
+    pit(X, Y).
 
 has_breeze(X1, Y1) :-
-    todo.
+    SizeX = 5,
+    SizeY = 5,
+    ( Y1 < SizeY, Up is Y1 + 1,    pit(X1, Up)
+    ; Y1 > 1,     Down is Y1 - 1,  pit(X1, Down)
+    ; X1 < SizeX, Left is X1 + 1,  pit(Left, Y1)
+    ; X1 > 1,     Right is X1 - 1, pit(Right, Y1)
+    ).
 
+has_glitter(X1, Y1) :-
+    gold(X1, Y1).
 
 % Goals
-
+is_gold(X1, Y1) :-
+    has_glitter(X1, Y1).
 
 % Unclassified
 distance(X1, Y1, X2, Y2, D) :-
@@ -19,40 +43,7 @@ distance(X1, Y1, X2, Y2, D) :-
 adjacent(X1, Y1, X2, Y2) :-
     distance(X1, Y1, X2, Y2, 1).
 
-is_safe_tile() :-
-    is_safe().
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+is_safe_tile(X, Y) :-
+    is_safe(X, Y).
 
 
