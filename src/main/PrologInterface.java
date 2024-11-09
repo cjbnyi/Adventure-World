31119cbb9cd35e73a3b1dbc72303452a89ca5011
Immanuel Z. Umali <138665333+Immern@users.noreply.github.com>
2024-11-09 15:11:05 +0800

    added some code to queryNewInformation for now

--- src/main/PrologInterface.java
+++ src/main/PrologInterface.java
@@ -4,6 +4,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Map;
+import org.jpl7.Query;
 
 public class PrologInterface {
 
@@ -37,8 +38,7 @@ public class PrologInterface {
                     yHome = currColumn;
                 }
                 mapInfo[currRow][currColumn++] = (char) character;
-            }
-            else if (character == '\n') {
+            } else if (character == '\n') {
                 currRow++;
                 currColumn = 0;
             }
@@ -60,8 +60,22 @@ public class PrologInterface {
     // TODO
     /**
      * Queries the knowledge base for new information given a player move.
+     * Returns an integer representing what type block is in the given row-col
+     * location
      */
-    public void queryNewInformation() {
+
+    // not sure how this should be implemented because there is a possibility that a
+    // block is both a breeze and a gold
+    // possible solution would be for the function to accept a string of what to
+    // query
+    public void queryNewInformation(int curRow, int curCol) {
+        Query checkForPit = new Query("is_pit", curRow, curCol);
+        Query checkForBreeze = new Query("has_breeze", curRow, curCol);
+        Query checkForGold = new Query("is_gold", curRow, curCol);
+        Query checkForGlitter = new Query("has_glitter", curRow, curCol); // tbh di ko gets point ng glitter since same
+                                                                          // location lang naman sa gold
+        Query checkForHome = new Query("is_home", curRow, curCol);
+        Query checkIfSafe = new Query("is_safe", curRow, curCol);
 
     }
 
@@ -78,27 +92,27 @@ public class PrologInterface {
      * Updates the knowledge base given new information.
      */
 
-     /*
-      * CURRENT IMPLEMENTATION:
-      * Finds home, gold, and pits then adds to the knowledge_base.pl
-      */
+    /*
+     * CURRENT IMPLEMENTATION:
+     * Finds home, gold, and pits then adds to the knowledge_base.pl
+     */
     public static void updateKnowledgeBase(char[][] mapInfo, int numRows, int numColum) {
         FileWriter writer = new FileWriter("knowledge_base.pl", false);
         writer.write("");
 
-        for(int i=0; i<numRows; i++){
-            for(int j=0; j<numCol; j++){
-                switch(mapInfo[i][j]){
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCol; j++) {
+                switch (mapInfo[i][j]) {
                     // currently uses 1-based index in knowledge base
                     // update if necessary
                     case 'P':
-                        writer.write("pit(%d, %d).", i+1, j+1);
+                        writer.write("pit(%d, %d).", i + 1, j + 1);
                         break;
                     case 'G':
-                        writer.write("gold(%d, %d).", i+1, j+1);
+                        writer.write("gold(%d, %d).", i + 1, j + 1);
                         break;
                     case 'H':
-                        writer.write("home(%d, %d).", i+1, j+1);
+                        writer.write("home(%d, %d).", i + 1, j + 1);
                         break;
                     default:
                         break;
